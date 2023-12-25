9cf5faf9e5649d29b6abf0824cb95915ab3706a6
hmdmohsinali <mohsin.201903372@gcuf.edu.pk>
2023-12-25 21:00:13 +0500

    add: necessary packages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from './components/navbar/Navbar';
+import  Home from './pages/home/Home';
+import Favorites  from './pages/favorites/Favorites';
+import  Details  from './pages/details/Details';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='min-h-screen p-6 bg-white text-gray-600 text-lg'>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/favorites' element={<Favorites/>} />
+          <Route path='/recipe-item/:id' element={<Details/>} />
+        </Routes>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

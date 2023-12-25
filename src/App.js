dd579afa20cbba7f1d005646fdba412017810599
hmdmohsinali <mohsin.201903372@gcuf.edu.pk>
2023-12-25 21:43:14 +0500

    add: navbar created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
-import { Navbar } from './components/navbar/Navbar';
+import Navbar from './components/navbar/Navbar';
 import  Home from './pages/home/Home';
 import Favorites  from './pages/favorites/Favorites';
 import  Details  from './pages/details/Details';
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-export const Navbar = () => {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
+      <h2 className="text-2xl font-semibold">
+        <NavLink
+          to={"/"}
+          className="text-black hover:text-gray-700 duration-300"
+        >
+          FoodRecipie
+        </NavLink>
+      </h2>
+      <form>
+        <input
+          type="text"
+          name="search"
+          placeholder="Enter items or food name"
+          className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
+        />
+      </form>
+      <ul className="flex gap-5 ">
+        <li>
+          <NavLink
+            to={"/"}
+            className="text-black hover:text-gray-700 duration-300"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/favorites"}
+            className="text-black hover:text-gray-700 duration-300"
+          >
+            Favorites
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
